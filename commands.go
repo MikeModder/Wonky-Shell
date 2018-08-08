444e5d01787819093f173926eef816ab589c32fc
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-08-08 14:23:18 -0400

    Revamp commands, they're not in a giant switch now (thanks JoshuaDoes)

---
+++ commands.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// Command command
+type Command struct {
+	Help     string
+	Function func([]string)
+	Args     []Arg
+	ReqArgs  []string
+}
+
+// Arg command arg
+type Arg struct {
+	Name string
+	Type string
+	Desc string
+}
+
+// Commands map of commands
+var Commands = make(map[string]*Command)
+
+// InitCommands init all commands
+func InitCommands() {
+	// Setup all the commands here
+	// No arguments commands
+	Commands["test"] = &Command{Help: "test", Function: testCmd}
+
+	// 1+ argument commands
+	Commands["help"] = &Command{
+		Help:     "Help!",
+		Function: helpCmd,
+		Args: []Arg{
+			{Name: "command", Type: "string", Desc: "option command to show help for"},
+		},
+	}
+	Commands["echo"] = &Command{
+		Help:     "Repeat after me!",
+		Function: echoCmd,
+		ReqArgs:  []string{"text"},
+		Args: []Arg{
+			{Name: "text", Type: "string", Desc: "What do I repeat"},
+		},
+	}
+	Commands["exit"] = &Command{
+		Help:     "Exit the shell",
+		Function: exitCmd,
+		Args: []Arg{
+			{Name: "code", Type: "int", Desc: "optional code to exit with"},
+		},
+	}
+}
+
+// CallCommand call a command
+func CallCommand(name string, args []string) {
+	if c, exist := Commands[name]; exist {
+		if len(args) >= len(c.ReqArgs) {
+			c.Function(args)
+			return
+		}
+		fmt.Printf("Not enough parameters for %s: %d required, %d passed\n", name, len(c.ReqArgs), len(args))
+		printCommandUsage(name)
+		return
+	}
+	fmt.Printf("%s is not a recognized command!\n", name)
+	return
+}
+
+func printCommandUsage(name string) {
+	c := Commands[name]
+	fmt.Printf("Usage for %s:\n %s %s\n  Parameters:\n", name, name, strings.Join(c.ReqArgs, " "))
+	for i := 0; i < len(c.Args); i++ {
+		fmt.Printf("  %s (%s) - %s\n", c.Args[i].Name, c.Args[i].Type, c.Args[i].Desc)
+	}
+	return
+}
+
+func testCmd(args []string) {
+	fmt.Println("Test command ok!")
+}
+
+func exitCmd(args []string) {
+	var e error
+	code := 0
+	if len(args) > 0 {
+		code, e = strconv.Atoi(args[0])
+		if e != nil {
+			code = 0
+			fmt.Printf("%s is not a valid int, exiting with %d\n", args[0], code)
+		}
+	}
+	fmt.Printf("Exiting with code %d, bye!\n", code)
+	os.Exit(code)
+}
+
+func echoCmd(args []string) {
+	fmt.Println(strings.Join(args, " "))
+}
+
+func helpCmd(args []string) {
+	if len(args) >= 1 {
+		cmd := args[0]
+		if _, exist := Commands[cmd]; exist {
+			printCommandUsage(cmd)
+			return
+		}
+		fmt.Printf("Connot get help for non existant command (%s)\n", cmd)
+		return
+	}
+	var commandNames []string
+	for cKey := range Commands {
+		commandNames = append(commandNames, cKey)
+	}
+	sort.Strings(commandNames)
+	for _, commandName := range commandNames {
+		c := Commands[commandName]
+		fmt.Printf("%s - %s\n", commandName, c.Help)
+	}
+	return
+}
--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"runtime"
-	"strconv"
 	"strings"
 
 	"github.com/c-bata/go-prompt"
@@ -13,14 +10,10 @@ import (
 var Version, GitCommit, BuildDate string
 var AppName = "HaxShell"
 
-/*var commands []prompt.Suggest = []prompt.Suggest{
-{Text: "exit", Description: "exit the program"},
-{Text: "echo", Description: "repeat text given"},
-{Text: "whoami", Description: "what user am I?"},
-{Text: "run", Description: "directly run a file"}}*/
-
 func main() {
 	fmt.Printf("%s v%s-%s (built %s)\n", AppName, Version, GitCommit, BuildDate)
+	fmt.Printf("Setting up...\n")
+	InitCommands()
 	p := prompt.New(executor, completer,
 		prompt.OptionPrefix("hax> "),
 		prompt.OptionTitle(fmt.Sprintf("%s v%s-%s (built %s)", AppName, Version, GitCommit, BuildDate)))
@@ -29,46 +22,11 @@ func main() {
 
 func executor(in string) {
 	arr := strings.Split(in, " ")
-	cmd := strings.ToLower(arr[0])
+	cmd := arr[0]
 	args := arr[1:]
-	handleCommand(cmd, args)
+	CallCommand(cmd, args)
 }
 
 func completer(in prompt.Document) []prompt.Suggest {
 	return []prompt.Suggest{}
-}
-
-func handleCommand(c string, args []string) {
-	switch c {
-	case "exit":
-		var e error
-		code := 0
-		if len(args) > 0 {
-			code, e = strconv.Atoi(args[0])
-			if e != nil {
-				code = 0
-			}
-		}
-		fmt.Printf("Exiting with code %d, bye!\n", code)
-		os.Exit(code)
-	case "echo":
-		fmt.Println(strings.Join(args, " "))
-	case "whoami":
-		fmt.Printf("You are %s running HaxShell\n", os.Getenv("USER"))
-	case "exist":
-		f, e := os.Stat(args[0])
-		if e != nil {
-			fmt.Printf("%s does not exist or you don't have permission to see it,\nstat error: %s\n", args[0], e.Error())
-			return
-		}
-		fmt.Printf("%s exists, directory: %t\nsize: %d\n", f.Name(), f.IsDir(), f.Size())
-	case "os":
-		fmt.Printf("You are running %s (arch: %s, %s)\n", runtime.GOOS, runtime.GOARCH, runtime.Version())
-	case "version":
-		fmt.Printf("%s v%s (git: %s, built %s)\n", AppName, Version, GitCommit, BuildDate)
-	case "help":
-		fmt.Printf("exit <code>\necho <text>\nwhomai\nexist <path>\nos\nversion\nhelp\n")
-	default:
-		fmt.Printf("%s is not a valid command!\n", c)
-	}
 }
